8047194d02ba475fc689e811c7e83f5113916457
Miguel CS Vanzela <miguelclaudiodasilvavanzela@gmail.com>
2023-10-20 15:09:32 -0300

    Desafio criando um componente simples

---
+++ App/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import HelloWorld from './components/1compProps/HelloWorld'
+
+function App() {
+  return (
+    <>
+      <HelloWorld />
+    </>
+  )
+}
+
+export default App
---
+++ App/src/components/1compProps/HelloWorld.jsx
@@ -0,0 +1,12 @@
+// Componente 1 - crie um componente com um paragrafo
+
+const HelloWorld = () => {
+
+    return(
+        <>
+            <p>Olá, mundão vei sem portera!</p>
+        </>
+    )
+}
+
+export default HelloWorld
